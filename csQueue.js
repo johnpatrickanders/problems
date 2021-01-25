ccfd1f2902f569726f3f44a35860dbe77ac2078d
johnpatrickanders <johnpatrickanders@gmail.com>
2021-01-25 17:40:57 -0500

    complete first try at queue methods

--- csQueue.js
+++ csQueue.js
@@ -1,13 +1,44 @@
 // In JavaScript, any function can return an object.
 // When it does so without the new keyword, it’s a factory function.
 
+// a request is a funciton that returns a promise resolving to a string
+
 function createRequestQueue() {
+  const table = {};
+  const queue = [];
   return {
-    enqueue() {
-      return "hi"
+    enqueue(id, req) {
+      // const promise = new Promise(req);
+      table[id] = req;
+      queue.push(req);
+      return req;
+    },
+    cancel(id) {
+      if (table[id]) {
+        for (let i = 0; i < queue.length; i++) {
+          const promise = queue[i];
+          if (promise === table[id]) {
+            queue.splice(id, 1);
+            delete table[id];
+          }
+        }
+      }
+    },
+    processNext() {
+      const promise = queue.shift();
+      try {
+        promise.resolve();
+        return true;
+      } catch {
+        console.log("your promise had an error");
+        return false;
+      }
+    },
+    size() {
+      return queue.length;
     }
   }
 }
 
 const req = createRequestQueue();
-console.log(req.enqueue())
+console.log(req.enqueue(1, () => new Promise(() => "hi")))
